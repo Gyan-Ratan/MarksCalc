f9203d11dad748c82f52010a260dfebbe5381468
greko <gyan2020cs093@abesit.edu.in>
2022-01-05 19:12:42 +0530

    added varibles

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,6 +5,27 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var button1: android.widget.Button
+    lateinit var button2: android.widget.Button
+    lateinit var button3: android.widget.Button
+    lateinit var button4: android.widget.Button
+    lateinit var button5: android.widget.Button
+    lateinit var button6: android.widget.Button
+    lateinit var button7: android.widget.Button
+    lateinit var button8: android.widget.Button
+    lateinit var button9: android.widget.Button
+    lateinit var button0: android.widget.Button
+    lateinit var button00: android.widget.Button
+    lateinit var buttonpercentage: android.widget.Button
+    lateinit var buttonclear: android.widget.Button
+    lateinit var buttondot: android.widget.Button
+    lateinit var buttonequal: android.widget.Button
+    lateinit var buttonadd: android.widget.Button
+    lateinit var buttonsub: android.widget.Button
+    lateinit var buttonmul: android.widget.Button
+    lateinit var buttondivide: android.widget.Button
+    lateinit var buttonbackspace: android.widget.Button
+
 
 
 
